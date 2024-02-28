b822c7bda0b1860e38bbe9ffb006492d28242f72
karti <kartikparsodkar643@gmail.com>
2024-02-29 00:32:36 +0530

    Corrected the code and found out new way to run the training_pipeline script will add it in readme

--- src/logger.py
+++ src/logger.py
@@ -27,3 +27,19 @@ def setup_logging(log_file=r'D:\DiamondPricePrediction\app.log'):
 
 # Call setup_logging when this module is imported
 setup_logging()
+
+# import logging 
+# import os
+# from datetime import datetime
+
+# LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+# logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+# os.makedirs(logs_path,exist_ok=True)
+
+# LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+# logging.basicConfig(
+#     filename=LOG_FILE_PATH,
+#     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+#     level=logging.INFO
+# )
